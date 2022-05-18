1d18eb048859c31549e41bfed228075f868338c7
Philemon Boakye <lemonsyntax@gmail.com>
2022-05-18 22:48:12 +0000

    Created the random generator and messages varaiables

---
+++ main.js
@@ -0,0 +1,11 @@
+
+//Generate random number 
+
+const GenerateRandomNumber = (number) => Math.floor(Math.random() * number);
+
+const messages = {
+    "fisrtMessage": ['lawyer', 'musicain','footballer'],
+    "secondMessage": ['armed robber', 'pick pocket', 'serial killer'],
+    "thirdMessage": ['amazing', 'beautiful', 'sexy'],
+    "fourthMessage": ['ugly', 'dirty', 'nuissance']
+};
